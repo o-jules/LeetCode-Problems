d99b2ee477e10de5da5b491ec1daaec32e1ae958
o-jules <jules3in@gmail.com>
2017-09-07 15:37:36 +0800

    add: plus-one

---
+++ 66 plus-one/c/main.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+vector<int> plusOne(vector<int> &digits) {
+  int i = digits.size() - 1;
+  if (i < 0)
+    return {1};
+
+  vector<int> res;
+  int j = 0,
+      f = 1;
+  for (; i >= 0; i--) {
+    j = digits[i] + f;
+    if (j >= 10) {
+      f = 1;
+      j -= 10;
+    } else {
+      f = 0;
+    }
+    res.insert(res.begin(), 1, j);
+  }
+
+  if (f != 0)
+    res.insert(res.begin(), 1, f);
+
+  return res;
+}
+
+int main(void) {
+  vector<int> digits = {9,9,9};
+
+  vector<int> num = plusOne(digits);
+  for (int &i: num) {
+    cout << i << " ";
+  }
+  cout << endl;
+}
