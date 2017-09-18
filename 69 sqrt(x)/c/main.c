12cb93c8250d22ceb5658b3cabf0582864f709e6
o-jules <jules3in@gmail.com>
2017-09-18 17:07:12 +0800

    update: sqrt(x)

--- 69 sqrt(x)/c/main.c
+++ 69 sqrt(x)/c/main.c
@@ -12,14 +12,26 @@ float mySqrt(int x) {
     return 0;
 
   float a = x;
-  while (fabs(a*a - x) >= 1e-6) {
-    a = a - (a*a - x)/(2*a);
+  float prev = -1;
+
+  while (fabs(pow(a, 2) - (float)x) > 1e-4) {
+    a = (a + (float)x / a)/2.0;
+    if (prev == a) {  break; }
+    prev = a;
   }
 
   return a;
 };
 
+void test(int x) {
+  printf("sqrt(%d) = %f\n", x, mySqrt(x));
+}
+
 int main(void) {
-  int x = 2;
-  printf("%f", mySqrt(x));
+  //test(2);
+  //test(3);
+  //test(8192);
+
+  // 极值测试：
+  test(2147395599); //Leetcode要求输出 46339， 当前是 46340
 }
