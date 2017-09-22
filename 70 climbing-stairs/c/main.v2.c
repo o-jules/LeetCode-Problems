251e224a9f669c31d8bd7355dd321c0615f1d1b3
o-jules <jules3in@gmail.com>
2017-09-22 15:27:07 +0800

    update: climb-stairs, readme.md

--- 70 climbing-stairs/c/main.v2.c
+++ 70 climbing-stairs/c/main.v2.c
@@ -12,13 +12,13 @@ int climbStairs(int n) {
     }
 
     int arr[n+1];
-    arr[0]=0;
-    arr[1]=1;
-    arr[2]=2;
+    arr[0] = 0;
+    arr[1] = 1;
+    arr[2] = 2;
 
     int i;
-    for(i=3; i<=n; i++) {
-        arr[i] = arr[i-1]+arr[i-2];
+    for(i = 3; i <= n; i++) {
+        arr[i] = arr[i-1] + arr[i-2];
     }
 
     return arr[n];
