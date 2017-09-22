481cca099c8c0295ad37918da988fe5808d488fc
o-jules <jules3in@gmail.com>
2017-09-22 15:04:23 +0800

    update: climb-stairs

---
+++ 70 climbing-stairs/c/main.v2.c
@@ -0,0 +1,26 @@
+/**
+ * Leetcode最优解
+ */
+
+int climbStairs(int n) {
+    if(n == 1) {
+      return 1;
+    }
+
+    if(n == 2) {
+      return 2;
+    }
+
+    int arr[n+1];
+    arr[0]=0;
+    arr[1]=1;
+    arr[2]=2;
+
+    int i;
+    for(i=3; i<=n; i++) {
+        arr[i] = arr[i-1]+arr[i-2];
+    }
+
+    return arr[n];
+  }
+
