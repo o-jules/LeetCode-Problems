6fd67026d1862e78de8edda94cf61f347dff5a4a
o-jules <jules3in@gmail.com>
2017-10-10 11:35:14 +0800

    add: counting-bits

---
+++ 338 counting-bits/js/main.js
@@ -0,0 +1,36 @@
+/**
+ * @param {number} num
+ * @return {number[]}
+ */
+var countBits = function(num) {
+  if (num <= 0) return [0]
+
+  let arr = new Array(num + 1).fill(0)
+  arr[1] = 1
+
+  let i = 2, j = 1
+  let k = 2
+
+  while (i <= num) {
+    if (i == k) {
+      arr[i] = 1
+      j = 1
+      k *= 2
+    } else {
+      arr[i] = 1 + arr[j]
+      j++
+    }
+
+    i++
+  }
+
+  return arr
+};
+
+// console.log(countBits(0))
+console.log(countBits(1))
+// console.log(countBits(2))
+console.log(countBits(3))
+console.log(countBits(4))
+console.log(countBits(5))
+console.log(countBits(6))
