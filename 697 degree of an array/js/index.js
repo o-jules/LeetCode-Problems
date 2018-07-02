00f843cc7e466ee1a6b5129552ebab2b0b37e26c
o-jules <jules3in@gmail.com>
2018-07-02 11:52:38 +0800

    update: 697 degree of an array

--- 697 degree of an array/js/index.js
+++ 697 degree of an array/js/index.js
@@ -4,31 +4,41 @@
  */
 var findShortestSubArray = function(nums) {
   const countMap = {};
+  let degree = 1;
 
-  for (let i = 0; i < nums.length; i++) {
-    const n = nums[i];
-    if (countMap[n]) {
-      countMap[n].count ++;
-      countMap[n].end = i;
+  nums.forEach((n, i) => {
+    const item = countMap[n];
+    if (item) {
+      item.count++;
+      item.end = i;
+      if (degree < item.count) {
+        degree = item.count;
+      }
     } else {
       countMap[n] = {
         count: 1,
         start: i - 1,
         end: i
+      };
+    }
+  });
+
+  if (degree === 1) {
+    return 1;
+  }
+
+  let min = Infinity;
+  Object.keys(countMap).forEach(key => {
+    const item = countMap[key]
+    if (item.count === degree) {
+      const len = item.end - item.start
+      if (min > len) {
+        min = len;
       }
     }
-  }
+  });
 
-  const sort = Object.keys(countMap).map(i =>  countMap[i]).sort((l,r) => r.count - l.count);
-  const degree = sort[0].count
-  if (degree === 1) {
-    return 1
-  }
-
-  const countList = sort.filter(i => i.count === degree).map(i => i.end - i.start);
-  return Math.min(...countList);
+  return min;
 };
 
-console.log(
-  findShortestSubArray([1,2,2,3,1,4,2])
-)
+console.log(findShortestSubArray([1, 2, 2, 3, 1, 4, 2]));
