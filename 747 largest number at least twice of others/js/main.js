fb523c937d42b7493c69f4daff382f7dc84afa8e
o-jules <jules3in@gmail.com>
2018-07-05 10:04:30 +0800

    add: 747 largest number at least twice of others

---
+++ 747 largest number at least twice of others/js/main.js
@@ -0,0 +1,43 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var dominantIndex = function(nums) {
+  const len = nums.length;
+
+  let largest = -Infinity;
+  let largestIndex = 0;
+
+  for (let i = 0; i < len; i++) {
+    if (nums[i] > largest) {
+      largest = nums[i];
+      largestIndex = i;
+    }
+  }
+
+  for (let i = 0; i < len; i++) {
+    if (nums[i] != largest) {
+      if (largest < nums[i] * 2) {
+        return -1;
+      }
+    }
+  }
+
+  return largestIndex;
+};
+
+console.log(
+  dominantIndex([4,3,1,0])
+)
+
+console.log(
+  dominantIndex([2,6,3,1,0])
+)
+
+console.log(
+  dominantIndex([4,2,1,0])
+)
+
+console.log(
+  dominantIndex([1])
+)
