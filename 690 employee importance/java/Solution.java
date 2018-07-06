39ff6466daa917f99ab42fa4d18d1a13f33c11cf
o-jules <jules3in@gmail.com>
2018-07-06 21:56:28 +0800

    add: 690 employee importance

---
+++ 690 employee importance/java/Solution.java
@@ -0,0 +1,27 @@
+/*
+// Employee info
+class Employee {
+    // It's the unique id of each node;
+    // unique id of this employee
+    public int id;
+    // the importance value of this employee
+    public int importance;
+    // the id of direct subordinates
+    public List<Integer> subordinates;
+};
+*/
+class Solution {
+    public int getImportance(List<Employee> employees, int id) {
+        int importance = 0;
+        for(Employee e: employees) {
+            if (e.id == id) {
+                importance = e.importance;
+                for (Integer i: e.subordinates) {
+                    importance += getImportance(employees, i);
+                }
+                break;
+            }
+        }
+        return importance;
+    }
+}
