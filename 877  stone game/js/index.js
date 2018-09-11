5bd648716c03c3255282d5f1f2216f1f7ccc0977
o-jules <jules3in@gmail.com>
2018-09-11 14:26:34 +0800

    add 877 stone game

---
+++ 877  stone game/js/index.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} piles
+ * @return {boolean}
+ */
+var stoneGame = function(piles) {
+  return true;
+};
+
+console.log(stoneGame([5, 3, 4, 5, 6, 2]))
+console.log(stoneGame([7,8,8,10]))
+
+/*
+var stoneGame = function(piles) {
+  return true;
+}
+*/
