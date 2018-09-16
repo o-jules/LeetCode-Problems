d38e7af73ba89b5a451f0a5f0ed7ca9e0dc6a86c
o-jules <jules3in@gmail.com>
2018-09-16 22:43:21 +0800

    update #796

--- 796 rotate string/c/main.c
+++ 796 rotate string/c/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <assert.h>
 
 #define bool int;
 #define true 1;
@@ -13,27 +14,42 @@ bool rotateString(char* A, char* B) {
   if (len == 0) {
     return true;
   }
+  if (len == 1) {
+    return A[0] == B[0];
+  }
+
+  int a, b;
   for (int i = 0; i < len; i++) {
-    /**/
-    int j = i, k = 0;
-    while (j < (len + i)) {
-      if (B[k] != A[j%len]) {
+    if (A[i] != B[0]) {
+      continue;
+    }
+
+    for(a = i, b = 0; a < len; a++, b++) {
+      if (A[a] != B[b]) {
         goto next;
       }
-      j++;
-      k++;
+    }
+    for (a = 0, b = len - i; a < i; a++, b++) {
+      if (A[a] != B[b]) {
+        goto next;
+      }
     }
     return true;
 
-    next:
-      continue;
+    next: ;
   }
 
   return false;
 }
 
 int main() {
-  printf("%d\n", rotateString("abcde", "cdeab"));
-  printf("%d\n", rotateString("abcde", "abced"));
-  printf("%d\n", rotateString("abcde", "abced"));
+  assert(rotateString("", "") == 1);
+  assert(rotateString("a", "a") == 1);
+  assert(rotateString("ab", "ab") == 1);
+  assert(rotateString("ab", "ba") == 1);
+  assert(rotateString("abcde", "cdeab") == 1);
+  assert(rotateString("abcde", "abced") == 0);
+  assert(rotateString("abcde", "abcde") == 1);
+
+  printf("All test cases are passed.\n");
 }
