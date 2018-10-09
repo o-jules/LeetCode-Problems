2f8c5b21da4eca0d8b14d81b14dea812219dfe19
o-jules <jules3in@gmail.com>
2018-10-09 21:26:23 +0800

    add better DP solution for #718

--- 718 maximum length of repeated subarray/js/index-dp2.js
+++ 718 maximum length of repeated subarray/js/index-dp2.js
@@ -1,4 +1,4 @@
-// 动态规划
+// 使用缓存的 动态规划
 
 /**
  * @param {number[]} A
@@ -10,11 +10,20 @@ var findLength = function(A, B) {
   const lb = B.length;
 
   let max = 0;
+  let m = 0;
+  const cache = [];
+  for (let i = 0; i < la; i++) {
+    if (!cache[i]) {
+      cache[i] = new Array(lb).fill(null);
+    }
+  }
+
   for (let i = 0; i < la; i++) {
     // prefix, (A, 0, i)
+
     for (let j = 0; j < lb; j++) {
       // prefix, (B, 0, j)
-      const m = lcs(A, B, i, j);
+      m = lcs(A, B, i, j, cache);
       if (m > max) {
         max = m;
       }
@@ -24,13 +33,24 @@ var findLength = function(A, B) {
   return max;
 };
 
-function lcs(A, B, i, j) {
+function lcs(A, B, i, j, cache) {
   if (i < 0 || j < 0) {
     return 0;
   }
+
   if (A[i] === B[j]) {
-    return lcs(A, B, i - 1, j - 1) + 1;
+    if (i - 1 < 0 || j - 1 < 0) {
+      return 1;
+    }
+    if (cache[i - 1][j - 1] === null) {
+      const m = lcs(A, B, i - 1, j - 1, cache);
+      cache[i - 1][j - 1] = m;
+      return m + 1;
+    } else {
+      return cache[i - 1][j - 1] + 1;
+    }
   }
+
   return 0;
 }
 
