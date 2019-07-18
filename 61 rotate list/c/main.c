4c833e03372a44e8207f5af78bd546e5f1866b55
o-jules <jules3in@gmail.com>
2019-07-19 00:59:37 +0800

    add: 61 rotate list

--- 61 rotate list/c/main.c
+++ 61 rotate list/c/main.c
@@ -7,11 +7,11 @@
  */
 
 struct ListNode* rotateRight(struct ListNode* head, int k) {
-  if (head == NULL) {
+  if (head == NULL || head->next == NULL || k == 0) {
       return head;
   }
-  struct ListNode* p1 = head;
-  struct ListNode* p2 = head;
+  struct ListNode *p1, *p2;
+  p1 = p2 = head;
   int len = 0;
 
   while (k > 0) {
@@ -28,9 +28,11 @@ struct ListNode* rotateRight(struct ListNode* head, int k) {
     p1 = p1->next;
     p2 = p2->next;
   }
+  // now p2 is tail
   p2->next = head;
   p2 = p1->next;
   p1->next = NULL;
 
   return p2;
 }
+
