58d2f0ebde5f5d02b57774e2a20581ba9d0ad701
o-jules <jules3in@gmail.com>
2019-07-22 22:15:40 +0800

    update:143

--- 143 reorder list/c/main.c
+++ 143 reorder list/c/main.c
@@ -13,58 +13,44 @@
      struct ListNode *next;
  };
 
-void reorderList(struct ListNode* head){
-  if (head == NULL || head->next == NULL) {
+void reorderList(struct ListNode* head) {
+  if (!head || !head->next) {
     return;
   }
 
   // find middle node
-  struct ListNode *i = head;
-  struct ListNode *p = NULL;
-  unsigned char is_double = 0;
-  while(i != NULL) {
+  struct ListNode *i, *j, *k;
+  i = head;
+  j = NULL;
+  while(i) {
     i = i->next;
-    if (is_double == 1) {
-      p = p == NULL ? head: p->next;
-      is_double = 0;
-    } else {
-      is_double = 1;
+    j = j ? j->next : head;
+    if (i) {
+      i = i->next;
     }
   }
-  i = p;
-  p = p->next;
+  i = j;
+  j = j->next;
   i->next = NULL;
-  // now `p` is the middle
+  // now `j` is the middle
 
   // reverse the final half list
-  struct ListNode *t = p;
-  struct ListNode *n = p->next;
-  t->next = NULL;
-  while (n)
-  {
-    i = n->next;
-    n->next = t;
-    t = n;
-    n = i;
+  k = j->next;
+  j->next = NULL;
+  while (k) {
+    i = k->next;
+    k->next = j;
+    j = k;
+    k = i;
   }
-  // now t is the tail
+  // now `j` is the tail
 
-  unsigned char is_first_half = 1;
-  struct ListNode *first_half_head = head;
-  struct ListNode *second_half_head = t;
-  i = head;
-  while (first_half_head != NULL && second_half_head != NULL) {
-    if (is_first_half == 1) {
-      first_half_head = first_half_head->next;
-      i->next = second_half_head;
-      i = i->next;
-      is_first_half = 0;
-    } else {
-      second_half_head = second_half_head->next;
-      i->next = first_half_head;
-      i = i->next;
-      is_first_half = 1;
-    }
+  k = head;
+  while (j != NULL && k != NULL) {
+    i = k->next;
+    k->next = j;
+    j = i;
+    k = k->next;
   }
 
 }
@@ -76,7 +62,7 @@ int main() {
   list->next = NULL;
   struct ListNode *tail = list;
 
-  int count = 4;
+  int count = 2;
   for (int i = 2; i <= count; i++) {
     struct ListNode *node = (struct ListNode *)malloc(sizeof(struct ListNode));
     node->val = i;
