f032b6881af7c9a89e642a869aca11799637d1e5
o-jules <jules3in@gmail.com>
2019-08-16 23:40:25 +0800

    add:669 trim a binary search tree

---
+++ 669 trim a binary search tree/c/main.c
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+
+
+struct TreeNode* trimBST(struct TreeNode* root, int L, int R){
+    if (!root) return root;
+
+    if (root->val > R) return trimBST(root->left, L, R);
+    if (root->val < L) return trimBST(root->right, L, R);
+
+    root->left = trimBST(root->left, L, R);
+    root->right = trimBST(root->right, L, R);
+
+    return root;
+}
+
+
