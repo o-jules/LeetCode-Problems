596dd772961d367c781327f30b6ec518511c904d
o-jules <jules3in@gmail.com>
2019-08-22 18:58:27 +0800

    update:392 is subsequence[better code]

--- 392 is subsequence/main.c
+++ 392 is subsequence/main.c
@@ -1,17 +1,11 @@
 
 bool isSubsequence(char * s, char * t){
     char *p1 = s, *p2 = t;
-    while (*p1) {
-        while (*p2) {
-            if (*p1 == *p2++) {
-                p1++;
-                break;
-            }
+    while (*p1 && *p2) {
+        if (*p1 == *p2) {
+            p1++;
         }
-
-        if (!(*p2)) {
-            break;
-        }
+        p2++;
     }
 
 
